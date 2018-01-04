726fd35dca21b8209912704809d7b82843fd5917
Paulo <psface1@gmail.com>
2018-01-04 02:50:13 -0200

    Making first test

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# bitcoin CLI
+# Bitcoin CLI
 
 Repository created only for studying purposes, you can see the original repository [right here](https://github.com/willianjusten/btc-converter)
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,14 @@
-import { expect } from 'chai';
+const expect = require('chai').expect;
+
+const exec = require('child_process').exec;
+const btcConverter = './src/main.js';
 
 describe('Main', () => {
-
+  it('should return Hello World', (done) => {
+    exec(btcConverter, (err, stdout, stderr) => {
+      if(err) throw err;
+      expect(stdout.replace('\n', '')).to.be.equal('Hello World');
+      done();
+    });
+  });
 });
