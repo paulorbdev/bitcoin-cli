68ce30f09632770a9fc8bc41bb448a437ffe8e17
Paulo <psface1@gmail.com>
2018-01-07 00:21:56 -0200

    Adding options

---
+++ bin/main.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+'use strict';
+
+var program = require('commander');
+var pkg = require('../package.json');
+var convertBTC = require('./ConvertBTC');
+
+program.version(pkg.version).description('Convert BTC to any currency').option('-C, --currency <>', 'Currency to be converted. (Default: USD)').option('-A, --amount <>', 'Value in bitcoin to be converted. (Default: 1)').parse(process.argv);
+
+console.log(convertBTC(program.currency, program.amount));
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 #!/usr/bin/env node
 const program = require('commander');
 const pkg = require('../package.json');
+const convertBTC = require('./ConvertBTC');
 
-program.version(pkg.version).parse(process.argv);
+program
+  .version(pkg.version)
+  .description('Convert BTC to any currency')
+  .option('-C, --currency <>', 'Currency to be converted. (Default: USD)')
+  .option('-A, --amount <>', 'Value in bitcoin to be converted. (Default: 1)')
+  .parse(process.argv);
+
+console.log(convertBTC(program.currency, program.amount));
