0ca7fa077003a369d0ea9f86f2d39cdee788ef5f
Paulo <psface1@gmail.com>
2018-01-08 23:13:44 -0200

    Making connection with the API

--- tests/ConvertBTC.spec.js
+++ tests/ConvertBTC.spec.js
@@ -1,13 +1,53 @@
 const chai = require('chai');
+const nock = require('nock');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
 const expect = chai.expect;
+
 const convertBTC = require('../src/ConvertBTC');
+chai.use(sinonChai);
 
 describe('ConvertBTC', () => {
-  it('should return USD as default currency and 1 as default amount', () => {
-    expect(convertBTC()).to.be.equal('1 BTC to USD = 2000.00');
+
+  let consoleStub;
+
+  const responseMock = {
+    "time": "2018-01-09 00:52:10",
+    "success": true,
+    "price": 15053.34
+  };
+
+  beforeEach(() => {
+    consoleStub = sinon.stub(console, 'log');
   });
 
-  it('should return BRL as currency and 10 as amount', () => {
-    expect(convertBTC('BRL', 10)).to.be.equal('10 BTC to BRL = 2000.00');
+  afterEach(() => {
+    console.log.restore();
+  });
+
+  it('should use USD as default currency and 1 as the default amount', (done) => {
+    nock('https://apiv2.bitcoinaverage.com')
+      .get('/convert/global')
+      .query({from: 'BTC', to: 'USD', amount: 1})
+      .reply(200, responseMock);
+
+      convertBTC();
+      setTimeout(() => {
+        expect(consoleStub).to.have.been.calledWith('1 BTC to USD = 15053.34');
+        done();
+      }, 300);
+  });
+
+  it('should use USD as default currency and 10 as the amount', (done) => {
+    nock('https://apiv2.bitcoinaverage.com')
+      .get('/convert/global')
+      .query({from: 'BTC', to: 'USD', amount: 10})
+      .reply(200, responseMock);
+
+      convertBTC('USD', 10);
+      setTimeout(() => {
+        expect(consoleStub).to.have.been.calledWith('10 BTC to USD = 15053.34');
+        done();
+      }, 300);
   });
 });
