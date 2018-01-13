e3d20aa24814ae46d83fc28b15f880ba9e98d5a3
Paulo <psface1@gmail.com>
2018-01-12 22:58:46 -0200

    Hadling errors

--- src/ConvertBTC.js
+++ src/ConvertBTC.js
@@ -4,7 +4,14 @@ function convertBTC(currency = 'USD', amount = 1){
   const url = `https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=${currency}&amount=${amount}`;
 
   request(url, (err, response, body) => {
-    let apiResponse = JSON.parse(body);
+    let apiResponse;
+
+    try {
+      apiResponse = JSON.parse(body);
+    } catch(parseError) {
+      console.log('Something went wrong with the API. Try in a few minutes');
+      return parseError;
+    }
     console.log(`${amount} BTC to ${currency} = ${apiResponse.price}`);
   });
 }
--- tests/ConvertBTC.spec.js
+++ tests/ConvertBTC.spec.js
@@ -38,7 +38,7 @@ describe('ConvertBTC', () => {
       }, 300);
   });
 
-  it('should use USD as default currency and 10 as the amount', (done) => {
+  it('should use USD as currency and 10 as a amount', (done) => {
     nock('https://apiv2.bitcoinaverage.com')
       .get('/convert/global')
       .query({from: 'BTC', to: 'USD', amount: 10})
@@ -47,6 +47,32 @@ describe('ConvertBTC', () => {
       convertBTC('USD', 10);
       setTimeout(() => {
         expect(consoleStub).to.have.been.calledWith('10 BTC to USD = 15053.34');
+        done();
+      }, 300);
+  });
+
+  it('should use BRL as currency and 1 as the default amount', (done) => {
+    nock('https://apiv2.bitcoinaverage.com')
+      .get('/convert/global')
+      .query({from: 'BTC', to: 'BRL', amount: 1})
+      .reply(200, responseMock);
+
+      convertBTC('BRL');
+      setTimeout(() => {
+        expect(consoleStub).to.have.been.calledWith('1 BTC to BRL = 15053.34');
+        done();
+      }, 300);
+  });
+
+  it('should message user when API reply with a error', (done) => {
+    nock('https://apiv2.bitcoinaverage.com')
+      .get('/convert/global')
+      .query({from: 'BTC', to: 'BRL', amount: 1})
+      .replyWithError('Error');
+
+      convertBTC('BRL');
+      setTimeout(() => {
+        expect(consoleStub).to.have.been.calledWith('Something went wrong with the API. Try in a few minutes');
         done();
       }, 300);
   });
